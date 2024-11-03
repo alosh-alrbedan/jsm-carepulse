d6b0110dd8a95fa2a1a78167002b2ef9651a3f65
alosh-alrbedan <your-email@example.com>
2024-11-03 20:22:56 +0300

    this is the lasttt adjust

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
     },
     eslint: {
         ignoreBuildErrors: true
+    },
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+            path: false,
+            os: false
+        };
+        return config;
     }
 };
 
